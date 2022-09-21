7cbf44e50bdba9a815c4590445480f6c8e89c991
Halkira <58034337+Halkira@users.noreply.github.com>
2022-09-22 01:05:59 +0200

    Soirée WT

--- Developpement.c
+++ Developpement.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main()
-{
-    printf("BZ TA MERE LE COMPILEUR");
+int main() {
+    printf("BZ TA MERE LE COMPILEUR\n");
+    printf("test\n");
+    printf("%.2f\n", 10.1515151515415);
+
+    int test;
+    printf("Enter an integer: ");
+    scanf("%d", &test);
+    printf("Input: %d", test);
 }
