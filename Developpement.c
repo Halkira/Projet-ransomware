6b15e98cf8d38725ac850c3232fa56b4a2afa2c4
Halkira <58034337+Halkira@users.noreply.github.com>
2022-09-23 13:14:06 +0200

    Fonctions ajoutées au projet avec la librairie openssl. Installation d'openssl requis ?

--- Developpement.c
+++ Developpement.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <openssl/ssl.h>
 
 int main() {
     printf("test\n");
@@ -7,8 +8,40 @@ int main() {
     int test;
     printf("Enter an integer:");
     scanf("%d", &test);
-    printf("Input: %d", test);
+    printf("Input: %d\n", test);
 
     int a = 5;
     printf("\n%d", a);
+}
+
+char connection (IP_attaquant, IP_victime, port){
+
+}
+
+char read_file (filename){
+
+}
+
+char file_location (path){
+
+}
+
+char gen_key (){
+
+}
+
+char send_key (key){
+
+}
+
+char crypt (key, filename){
+
+}
+
+char decrypt (key, filename){
+
+}
+
+char delete_key (path_key){
+
 }
