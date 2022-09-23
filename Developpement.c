7d58d3043ad79e22e441b30bb889275ad12a56cd
Victor <etu49391@henallux.be>
2022-09-23 11:15:49 +0200

    ZIZI

--- Developpement.c
+++ Developpement.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    printf("BZ TA MERE LE COMPILEUR\n");
     printf("test\n");
     printf("%.2f\n", 10.1515151515415);
 
     int test;
-    printf("Enter an integer: ");
+    printf("Enter an integer:");
     scanf("%d", &test);
     printf("Input: %d", test);
+
+    int a = 5;
+    printf("\n%d", a);
 }
