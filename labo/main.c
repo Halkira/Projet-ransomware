c3d5be6fbb2a52e874ce3ce2e532eeca1a6cdfc2
Helvor <legros.victor@hotmail.com>
2022-10-07 11:12:53 +0200

    le code pour opendir vient de c-for-dummies.com

--- labo/main.c
+++ labo/main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <dirent.h>
 
 /// \param arr Array of Char
 /// \param size Size of the array
 /// \param i Index of the current iteration
+
+
+/*
+/// Fonction pour print un array
 
 void printCharArray(char arr[], int size, unsigned int i)
 {
@@ -14,9 +19,40 @@ void printCharArray(char arr[], int size, unsigned int i)
         printCharArray(arr, size, i);
     }
 }
+*/
+
 
 int main(void)
 {
-    char arr[] = {'A', 'B', 'C', 'D'};
-    printCharArray(arr, 4, 0);
+    /*char arr[] = {'A', 'B', 'C', 'D'};
+    printCharArray(arr, 4, 0);*/
+
+    /// Code recopié depuis c-for-dummies.com
+
+    DIR *folder;
+    struct dirent *entry;
+    int files = 0;
+
+    folder = opendir(".");
+    if(folder == NULL)
+    {
+        perror("Unable to read directory");
+        return(1);
+    }
+
+    while( (entry=readdir(folder)) )
+    {
+        files++;
+        printf("File %3d: %s\n",
+               files,
+               entry->d_name
+        );
+    }
+
+    closedir(folder);
+
+    return(0);
+
+
+
 }
