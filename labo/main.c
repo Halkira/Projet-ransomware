28dd76bfdd2de295f566ea2e2994da4360a51500
Victor <etu49391@henallux.be>
2022-10-20 15:01:47 +0200

    add malloc and free the memory

--- labo/main.c
+++ labo/main.c
@@ -12,18 +12,14 @@ void list_dir(const char* path) {
     ///Check if there is an entry or not
     while ((entry = readdir(directory)) != NULL) {
 
-        ///d_type is for checking if it's a dir or not (DT_DIR for dir)
-        ///strcomp is for comparing the string because when there is the file "." or ".." the recursive doesn't work and make a loop
         if (entry->d_type == DT_DIR && strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
-            unsigned short int lenght = (strlen(path)+1+strlen(entry->d_name)+1);
+            unsigned short int lenght = strlen(path)+1+strlen(entry->d_name)+1;
             char *buffer = (char *)malloc(lenght * sizeof(char));
-            ///we don't know the lenght of the path so we're setting that to 250
-            ///we concatenate the path to the path_files variable
-            strcat(buffer, path);
-            strcat(buffer, "/");
-            strcat(buffer, entry->d_name);
-            ///call the function for recursivity
-            list_dir(buffer);
+            if (buffer != NULL){
+                snprintf(buffer, lenght, "%s/%s", path, entry->d_name);
+                list_dir(buffer);
+                free(buffer);
+            }
         } else {
             printf("%s/%s\n", path, entry->d_name);
         }
