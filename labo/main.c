2222a24c4254302b0210ecab717ad4994f0eb3f1
Helvor <etu49391@henallux.be>
2022-12-02 08:54:53 +0100

    open file in list_dir

--- labo/main.c
+++ labo/main.c
@@ -15,179 +15,185 @@ void list_dir(const char *path) {
     printf("Reading files in: %s\n", path);
 
     while ((entry = readdir(directory)) != NULL) {
-
         if (entry->d_type == DT_DIR && strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
-            unsigned short int lenght = strlen(path)+1+strlen(entry->d_name)+1;
-            char *buffer = (char *)malloc(lenght * sizeof(char));
-            if (buffer != NULL){
+            unsigned short int lenght = strlen(path) + 1 + strlen(entry->d_name) + 1;
+            char *buffer = (char *) malloc(lenght * sizeof(char));
+            if (buffer != NULL) {
                 snprintf(buffer, lenght, "%s/%s", path, entry->d_name);
                 list_dir(buffer);
                 free(buffer);
             }
-        } else if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0){
+        } else if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
             printf("%s/%s\n", path, entry->d_name);
+            unsigned short int lenght = strlen(path) + 1 + strlen(entry->d_name) + 1;
+            char *buffer = (char *) malloc(lenght * sizeof(char));
+            if (buffer != NULL) {
+                snprintf(buffer, lenght, "%s/%s", path, entry->d_name);
+                if (buffer == NULL){
+                    printf("Can't open the file\n");
+                }
+                free(buffer);
+            }
+
         }
+
+        closedir(directory);
+    }
+}
+
+    void handleErrors(void) {
+        ERR_print_errors_fp(stderr);
+        abort();
     }
 
-    closedir(directory);
-}
+    int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
+                unsigned char *iv, unsigned char *ciphertext) {
+        EVP_CIPHER_CTX *ctx;
 
-void handleErrors(void)
-{
-    ERR_print_errors_fp(stderr);
-    abort();
-}
+        int len;
 
-int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
-            unsigned char *iv, unsigned char *ciphertext)
-{
-    EVP_CIPHER_CTX *ctx;
+        int ciphertext_len;
 
-    int len;
 
-    int ciphertext_len;
+        /* Create and initialise the context */
+        if (!(ctx = EVP_CIPHER_CTX_new()))
+            handleErrors();
 
-    /* Create and initialise the context */
-    if(!(ctx = EVP_CIPHER_CTX_new()))
-        handleErrors();
+        /*
+         * Initialise the encryption operation. IMPORTANT - ensure you use a key
+         * and IV size appropriate for your cipher
+         * In this example we are using 256 bit AES (i.e. a 256 bit key). The
+         * IV size for *most* modes is the same as the block size. For AES this
+         * is 128 bits
+         */
+        if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
+            handleErrors();
 
-    /*
-     * Initialise the encryption operation. IMPORTANT - ensure you use a key
-     * and IV size appropriate for your cipher
-     * In this example we are using 256 bit AES (i.e. a 256 bit key). The
-     * IV size for *most* modes is the same as the block size. For AES this
-     * is 128 bits
-     */
-    if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
-    handleErrors();
+        /*
+         * Provide the message to be encrypted, and obtain the encrypted output.
+         * EVP_EncryptUpdate can be called multiple times if necessary
+         */
+        if (1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
+            handleErrors();
+        ciphertext_len = len;
 
-    /*
-     * Provide the message to be encrypted, and obtain the encrypted output.
-     * EVP_EncryptUpdate can be called multiple times if necessary
-     */
-    if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
-    handleErrors();
-    ciphertext_len = len;
+        /*
+         * Finalise the encryption. Further ciphertext bytes may be written at
+         * this stage.
+         */
+        if (1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len))
+            handleErrors();
+        ciphertext_len += len;
 
-    /*
-     * Finalise the encryption. Further ciphertext bytes may be written at
-     * this stage.
-     */
-    if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len))
-    handleErrors();
-    ciphertext_len += len;
+        /* Clean up */
+        EVP_CIPHER_CTX_free(ctx);
 
-    /* Clean up */
-    EVP_CIPHER_CTX_free(ctx);
+        return ciphertext_len;
+    }
 
-    return ciphertext_len;
-}
+    int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
+                unsigned char *iv, unsigned char *plaintext) {
+        EVP_CIPHER_CTX *ctx;
 
-int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
-            unsigned char *iv, unsigned char *plaintext)
-{
-    EVP_CIPHER_CTX *ctx;
+        int len;
 
-    int len;
+        int plaintext_len;
 
-    int plaintext_len;
+        /* Create and initialise the context */
+        if (!(ctx = EVP_CIPHER_CTX_new()))
+            handleErrors();
 
-    /* Create and initialise the context */
-    if(!(ctx = EVP_CIPHER_CTX_new()))
-        handleErrors();
+        /*
+         * Initialise the decryption operation. IMPORTANT - ensure you use a key
+         * and IV size appropriate for your cipher
+         * In this example we are using 256 bit AES (i.e. a 256 bit key). The
+         * IV size for *most* modes is the same as the block size. For AES this
+         * is 128 bits
+         */
+        if (1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
+            handleErrors();
 
-    /*
-     * Initialise the decryption operation. IMPORTANT - ensure you use a key
-     * and IV size appropriate for your cipher
-     * In this example we are using 256 bit AES (i.e. a 256 bit key). The
-     * IV size for *most* modes is the same as the block size. For AES this
-     * is 128 bits
-     */
-    if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
-    handleErrors();
+        /*
+         * Provide the message to be decrypted, and obtain the plaintext output.
+         * EVP_DecryptUpdate can be called multiple times if necessary.
+         */
+        if (1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
+            handleErrors();
+        plaintext_len = len;
 
-    /*
-     * Provide the message to be decrypted, and obtain the plaintext output.
-     * EVP_DecryptUpdate can be called multiple times if necessary.
-     */
-    if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
-    handleErrors();
-    plaintext_len = len;
+        /*
+         * Finalise the decryption. Further plaintext bytes may be written at
+         * this stage.
+         */
+        if (1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len))
+            handleErrors();
+        plaintext_len += len;
 
-    /*
-     * Finalise the decryption. Further plaintext bytes may be written at
-     * this stage.
-     */
-    if(1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len))
-    handleErrors();
-    plaintext_len += len;
+        /* Clean up */
+        EVP_CIPHER_CTX_free(ctx);
 
-    /* Clean up */
-    EVP_CIPHER_CTX_free(ctx);
-
-    return plaintext_len;
-}
+        return plaintext_len;
+    }
 
 #define KEY_SIZE 32
 #define IV_SIZE 16
 
-int main(int argc, char * argv[]) {
+    int main(int argc, char *argv[]) {
 
-    printf("%d arguments \n", argc-1);
+        printf("%d arguments \n", argc - 1);
 
-    for(int i = 1; i<argc;i++){
-        printf("Path : %s\n", argv[i]);
-        if (strlen(argv[i]) <= PATH_MAX){
-            list_dir(argv[i]);
+        for (int i = 1; i < argc; i++) {
+            printf("Path : %s\n", argv[i]);
+            if (strlen(argv[i]) <= PATH_MAX) {
+                list_dir(argv[i]);
+            }
         }
+
+        /*
+        * Set up the key and iv. Do I need to say to not hard code these in a
+        * real application?:-)
+        */
+
+        unsigned char key[KEY_SIZE];
+        unsigned char iv[IV_SIZE];
+
+        RAND_bytes(key, KEY_SIZE);
+        RAND_bytes(iv, IV_SIZE);
+
+        /* Message to be encrypted */
+        unsigned char *plaintext =
+                (unsigned char *) "Text with phrases and word !";
+
+        /*
+         * Buffer for ciphertext. Ensure the buffer is long enough for the
+         * ciphertext which may be longer than the plaintext, depending on the
+         * algorithm and mode.
+         */
+        unsigned char ciphertext[128];
+
+        /* Buffer for the decrypted text */
+        unsigned char decryptedtext[128];
+
+        int decryptedtext_len, ciphertext_len;
+
+        /* Encrypt the plaintext */
+        ciphertext_len = encrypt(plaintext, strlen((char *) plaintext), key, iv,
+                                 ciphertext);
+
+        /* Do something useful with the ciphertext here */
+        printf("Ciphertext is:\n");
+        BIO_dump_fp(stdout, (const char *) ciphertext, ciphertext_len);
+
+        /* Decrypt the ciphertext */
+        decryptedtext_len = decrypt(ciphertext, ciphertext_len, key, iv,
+                                    decryptedtext);
+
+        /* Add a NULL terminator. We are expecting printable text */
+        decryptedtext[decryptedtext_len] = '\0';
+
+        /* Show the decrypted text */
+        printf("Decrypted text is:\n");
+        printf("%s\n", decryptedtext);
+
+        return (0);
     }
-
-    /*
-    * Set up the key and iv. Do I need to say to not hard code these in a
-    * real application?:-)
-    */
-
-    unsigned char key[KEY_SIZE];
-    unsigned char iv[IV_SIZE];
-
-    RAND_bytes(key, KEY_SIZE);
-    RAND_bytes(iv, IV_SIZE);
-
-    /* Message to be encrypted */
-    unsigned char *plaintext =
-            (unsigned char *)"Text with phrases and word !";
-
-    /*
-     * Buffer for ciphertext. Ensure the buffer is long enough for the
-     * ciphertext which may be longer than the plaintext, depending on the
-     * algorithm and mode.
-     */
-    unsigned char ciphertext[128];
-
-    /* Buffer for the decrypted text */
-    unsigned char decryptedtext[128];
-
-    int decryptedtext_len, ciphertext_len;
-
-    /* Encrypt the plaintext */
-    ciphertext_len = encrypt (plaintext, strlen ((char *)plaintext), key, iv,
-                              ciphertext);
-
-    /* Do something useful with the ciphertext here */
-    printf("Ciphertext is:\n");
-    BIO_dump_fp (stdout, (const char *)ciphertext, ciphertext_len);
-
-    /* Decrypt the ciphertext */
-    decryptedtext_len = decrypt(ciphertext, ciphertext_len, key, iv,
-                                decryptedtext);
-
-    /* Add a NULL terminator. We are expecting printable text */
-    decryptedtext[decryptedtext_len] = '\0';
-
-    /* Show the decrypted text */
-    printf("Decrypted text is:\n");
-    printf("%s\n", decryptedtext);
-
-
-    return (0);
-}
