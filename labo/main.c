5ee842ba48a886cbdd41601c39af5ffe505c9645
Helvor <etu49391@henallux.be>
2022-12-16 23:46:00 +0100

    ajout de free car il y avait un leak memory

--- labo/main.c
+++ labo/main.c
@@ -24,38 +24,20 @@ encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key, unsigne
 
     int ciphertext_len;
 
-
-    /* Create and initialise the context */
     if (!(ctx = EVP_CIPHER_CTX_new()))
         handleErrors();
 
-    /*
-     * Initialise the encryption operation. IMPORTANT - ensure you use a key
-     * and IV size appropriate for your cipher
-     * In this example we are using 256 bit AES (i.e. a 256 bit key). The
-     * IV size for *most* modes is the same as the block size. For AES this
-     * is 128 bits
-     */
     if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
         handleErrors();
 
-    /*
-     * Provide the message to be encrypted, and obtain the encrypted output.
-     * EVP_EncryptUpdate can be called multiple times if necessary
-     */
     if (1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
         handleErrors();
     ciphertext_len = len;
 
-    /*
-     * Finalise the encryption. Further ciphertext bytes may be written at
-     * this stage.
-     */
     if (1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len))
         handleErrors();
     ciphertext_len += len;
 
-    /* Clean up */
     EVP_CIPHER_CTX_free(ctx);
 
     return ciphertext_len;
@@ -69,37 +51,20 @@ int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key, u
 
     int plaintext_len;
 
-    /* Create and initialise the context */
     if (!(ctx = EVP_CIPHER_CTX_new()))
         handleErrors();
 
-    /*
-     * Initialise the decryption operation. IMPORTANT - ensure you use a key
-     * and IV size appropriate for your cipher
-     * In this example we are using 256 bit AES (i.e. a 256 bit key). The
-     * IV size for *most* modes is the same as the block size. For AES this
-     * is 128 bits
-     */
     if (1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
         handleErrors();
 
-    /*
-     * Provide the message to be decrypted, and obtain the plaintext output.
-     * EVP_DecryptUpdate can be called multiple times if necessary.
-     */
     if (1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
         handleErrors();
     plaintext_len = len;
 
-    /*
-     * Finalise the decryption. Further plaintext bytes may be written at
-     * this stage.
-     */
     if (1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len))
         handleErrors();
     plaintext_len += len;
 
-    /* Clean up */
     EVP_CIPHER_CTX_free(ctx);
 
     return plaintext_len;
@@ -228,6 +193,8 @@ void crypted_list_dir(const char *path, unsigned char *key, unsigned char *iv) {
 
                 if (res > 100000000 || res <= 0){
                     printf("Skipping '%s' - size : %ld bytes\n",file, res);
+                    free(buffer_start);
+                    free(buffer_crypted);
                     continue; //skip the function and continue
                     }
 
@@ -435,7 +402,7 @@ int main(int argc, char *argv[]) {
 
     t = clock() - t;
     double time_taken = ((double)t)/CLOCKS_PER_SEC;
-    printf("Time taken : %f seconds", time_taken);
+    printf("Time taken : %f seconds\n", time_taken);
 
     return (0);
 }
